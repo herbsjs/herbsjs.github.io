691eb323555720dfa88a44abb4ee817223b60fa9
judibo <juliadibo@gmail.com>
2021-04-10 20:09:35 -0300

    Fix grid of how it works

--- src/pages/indexComponents/examples/examples.js
+++ src/pages/indexComponents/examples/examples.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import styles from './styles.module.css';
-import theme from 'prism-react-renderer/themes/nightOwlLight';
-import {
-  LiveProvider,
-  LiveEditor
-} from 'react-live'
+import React from 'react'
+import styles from './styles.module.css'
+import theme from 'prism-react-renderer/themes/nightOwlLight'
+import { LiveProvider, LiveEditor } from 'react-live'
 
 const exampleEntity = `const Item = entity('Item', {
   id: field(Number),
@@ -59,28 +56,28 @@ const ret = await createList.run({name: 'Special To Do'})
 `
 
 export default function Examples() {
-  return (
-    <div className={styles.examples}>
-     <h2 className={styles.examplesTitle}>Domain</h2>
-     <div className={styles.exampleContent}>
-      <h3>ENTITIES</h3>
-      <div className={styles.exampleCode}>
-        <LiveProvider theme={theme} code={exampleEntity} >
-          <LiveEditor className={styles.examplesEditor} />
-        </LiveProvider>    
-      </div>
-    </div>
-    <div className={styles.exampleContent}>
-      <h3>USE CASES</h3>
-      <div className={styles.exampleCode}>
-        <LiveProvider  theme={theme} code={exampleUseCase}>
-          <LiveEditor className={styles.examplesEditor}/>
-        </LiveProvider>
-        </div>
-      </div>
-      <div className={styles.exampleArrow}>
-        <img src="img/arrow.png" alt="down-arrow" />
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.examples}>
+			<h2 className={styles.examplesTitle}>Domain</h2>
+			<div className={styles.exampleContent}>
+				<h3>ENTITIES</h3>
+				<div className={styles.exampleCode}>
+					<LiveProvider theme={theme} code={exampleEntity}>
+						<LiveEditor className={styles.examplesEditor} />
+					</LiveProvider>
+				</div>
+			</div>
+			<div className={styles.exampleContent}>
+				<h3>USE CASES</h3>
+				<div className={styles.exampleCode}>
+					<LiveProvider theme={theme} code={exampleUseCase}>
+						<LiveEditor className={styles.examplesEditor} />
+					</LiveProvider>
+				</div>
+			</div>
+			<div className={styles.exampleArrow}>
+				<img src='img/arrow.png' alt='down-arrow' />
+			</div>
+		</div>
+	)
 }
