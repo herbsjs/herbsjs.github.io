cb58d7b9cf85b8700a41e5fa641dff11f97b33b9
gabrielmellooliveira <gabrielmello3011@gmail.com>
2021-05-14 22:27:58 -0300

    Add herbs2rest

--- src/pages/indexComponents/howItWorks/howItWorks.js
+++ src/pages/indexComponents/howItWorks/howItWorks.js
@@ -76,21 +76,21 @@ export default function HowItWorks() {
 					<hr className={styles.line} />
 					<div className={styles.arrowRight} />
 				</div>
-				{/* <div className={styles.gridMiddleD}>
-        <hr className={styles.line}/>
-        <div className={styles.badgeMiddle}>
-          <Link to={useBaseUrl('docs/glues/herbs2rpl')} className={styles.badgeLink}>
-            Herbs 2 REPL
-          </Link>
-        </div>
-        <hr className={styles.line}/>
-        <div className={styles.arrowRight}/>
-      </div> */}
+				<div className={styles.gridMiddleD}>
+					<hr className={styles.line}/>
+					<div className={styles.badgeMiddle}>
+						<Link to={useBaseUrl('docs/glues/herbs2rest')} className={styles.badgeLink}>
+							Herbs 2 Rest
+						</Link>
+					</div>
+					<hr className={styles.line}/>
+					<div className={styles.arrowRight}/>
+				</div>
 				{/* GENERATED CODE */}
 				<div className={styles.gridBadgeA}>Herbs Shelf</div>
 				<div className={styles.gridBadgeB}>GraphQL</div>
 				<div className={styles.gridBadgeC}>Repository</div>
-				{/* <div className={styles.gridBadgeD}>REST-layer</div> */}
+				<div className={styles.gridBadgeD}>REST</div>
 			</div>
 			<div className={styles.legends}>
 				<div>
