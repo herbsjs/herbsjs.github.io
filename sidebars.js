390c759a42aea833680fbfbdca0931974bc989a7
gabrielmellooliveira <gabrielmello3011@gmail.com>
2021-05-15 20:03:16 -0300

    Update sidebars

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,8 @@
 const sidebar = {
   Introduction: ['getting-started','herbsjs', 'motivation',  'architecture', 'ecosystem'],
   Tutorial: ['new-project', 'create-entity', 'repository', 'create-usecase', 'graphql', 'herbsshelf', 'refactoring', 'security'],
-  UseCase: ['gettingStarted', 'features', 'steps', 'result', 'whatis'],
-  Entity: ['gettingStarted', 'features', 'validation', 'whatis'],
+  UseCase: ['gettingStarted', 'features', 'steps', 'result'],
+  Entity: ['gettingStarted', 'features', 'validation'],
   Glues: ['whatisglue', 'herbs2gql', 'herbs2rest', 'herbs2knex', 'herbsshelf', 'herbs2rpl', 'suma2text', 'herbs-snippets'],
 }
 
