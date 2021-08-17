aaf51966355136973b2b3eeb14ce277027407d40
VORTX\mm <mm@vortx.com.br>
2021-08-17 12:53:40 -0300

    feat: include environment config

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,5 +1,5 @@
 const environment = {
-    registryNpmUrl: 'https://registry.npmjs.com/-/v1/search'
+    registryNpmUrl: process.env.REACT_APP_NPM_REGISTRY_URL
 }
 
 export default environment
--- src/services/package-version.js
+++ src/services/package-version.js
@@ -2,7 +2,7 @@ import environment from "../config/environment"
 
 const packageVersion = async (name) => {
     try {
-        const request = await fetch(`${environment}?text=${name}&size=20`)
+        const request = await fetch(`${environment.registryNpmUrl}?text=${name}&size=20`)
         const { objects } = await request.json()
         return objects.find(pkg => pkg.package.name === name)
     } catch (error) {
