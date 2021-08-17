242def071218e948b1308cdb079cb3b5fe55cb2d
Jhonathan Soares <jhonathanmarolo@hotmail.com>
2021-08-17 11:41:59 -0300

    Update package-version.js

--- src/services/package-version.js
+++ src/services/package-version.js
@@ -1,6 +1,6 @@
 const packageVersion = async (name) => {
     try {
-        const request = await fetch(`http://registry.npmjs.com/-/v1/search?text=${name}&size=20`)
+        const request = await fetch(`https://registry.npmjs.com/-/v1/search?text=${name}&size=20`)
         const { objects } = await request.json()
         return objects.find(pkg => pkg.package.name === name)
     } catch (error) {
