96c72f2f86609f893a295e2cb6de6aa31cc701f3
VORTX\mm <mm@vortx.com.br>
2021-10-02 22:38:00 -0300

    refactor: improve window paramter validation

--- src/hooks/useAutoUserZoom.js
+++ src/hooks/useAutoUserZoom.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 const useAutoUserZoom = (window) => {
     const [userZoom, setUserZoom] = useState('normal')
     useEffect(() => {
-        if (typeof window !== 'undefined' && window.devicePixelRatio > 1)
+        if (window && window.devicePixelRatio > 1)
             setUserZoom(`${100 / (window.devicePixelRatio * 0.9)}%`)
     }, [window])
 
