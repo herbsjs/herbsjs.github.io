1cf0e4aaba07c4c1831a7ebe90b865504193721b
italo jose <italo.i@live.com>
2023-03-10 13:39:40 -0300

    content page

---
+++ docs/project/comunity-content.md
@@ -0,0 +1,11 @@
+---
+id: communitycontent
+title: Community content
+sidebar_label: Community content
+slug: /project/communitycontent
+custom_edit_url: null
+---
+
+import ContentCards from '../../src/pages/contentCards'
+
+<ContentCards />
---
+++ src/pages/contentCards/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core/";
+import Card from "../../components/imgMediaCard";
+import { contentList } from "../../../docs/project/contents";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+  },
+}));
+
+export default function AltCard() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+      >
+        {contentList.map((elem) => (
+          <Grid item xs={12} sm={6} md={6} key={contentList.indexOf(elem)}>
+            <Card {...elem} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+}
